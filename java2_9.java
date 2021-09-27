72e3e63e288633a7118d2650c4de7bc980ff5223
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-28 02:22:34 +0530

    java2_9.java

--- java2_9.java
+++ java2_9.java
@@ -1,70 +1,71 @@
 import java.io.*;
-    abstract class Furniture{
-        abstract String stress(int n);
-        abstract void fire(String Argument);
+abstract class Furniture{
+    abstract String stress(int n);
+    abstract String  fire(String Argument);
+}
+
+class Wooden extends Furniture{
+    String stress(int n){
+        String s = "ok";
+        String s1 ="Not";
+        if(n>=50)
+            return fire(s);
+        else
+            return fire(s1);
+    }
+    String fire(String Argument) {
+        String s = "ok";
+        String s1 = "Not";
+        if (Argument == s){
+            System.out.println("Wooden chair is safe from fire");
+        return "0";
     }
 
-    class Wooden extends Furniture{
-        String stress(int n){
-            String s = "ok";
-            String s1 ="Not";
-            if(n>=50)
-                return s;
-            else
-                return s1;
-        }
-        void fire(String Argument){
-            String s = "ok";
-            String s1 ="Not";
-            if(Argument==s)
-                System.out.println("Wooden chair is safe from fire");
-
-
-            else
-                System.out.println("Wooden table is not safe from fire");
+        else {
+            System.out.println("Wooden table is not safe from fire");
+            return "0";
         }
     }
+}
 
-    class Metal extends Furniture{
-        String stress(int n){
-            String s = "ok";
-            String s1 ="Not";
+class Metal extends Furniture{
+    String stress(int n){
+        String s = "ok";
+        String s1 ="Not";
 
-            if(n>=40)
-                return s;
+        if(n>=40)
+            return fire(s);
 
-            else
-                return s1;
+        else
+            return fire(s1);
+    }
+    String fire(String Argument){
+        String s = "ok";
+        String s1 ="Not";
+        if(Argument==s) {
+            System.out.println("Metal chair is safe from fire");
+            return "0";
         }
-        void fire(String Argument){
-            String s = "ok";
-            String s1 ="Not";
-            if(Argument==s)
-                System.out.println("Metal chair is safe from fire");
 
-            else
-                System.out.println("Metal table is not safe from fire");
+        else{
+            System.out.println("Metal table is not safe from fire");
+            return "0";
         }
     }
-    public class java2_9 {
-        public static void main (String[] args) {
-            Wooden chair1 = new Wooden();
-            Wooden table1 = new Wooden();
-            String f =  chair1.stress(50);
-            chair1.fire(f);
-             f =  table1.stress(20);
-            table1.fire(f);
+}
+public class java2_9
+{
+    public static void main (String[] args) {
+        Wooden chair1 = new Wooden();
+        Wooden table1 = new Wooden();
+        chair1.stress(50);
+        table1.stress(40);
 
-            Metal chair2 = new Metal();
-            Metal table2 = new Metal();
-            String f1 = chair2.stress(40);
-            chair2.fire(f1);
-            f1 = table2.stress(30);
-            table2.fire(f1);
 
-        }
+        Metal chair2 = new Metal();
+        Metal table2 = new Metal();
+        chair2.stress(40);
+        table2.stress(30);
+        
     }
-
-
-
-
+}
