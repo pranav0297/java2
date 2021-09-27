7283ff53f52d6bea158c5172287809750e013cbd
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-09-28 02:06:33 +0530

    java2_1.java

--- java2_1.java
+++ java2_1.java
@@ -1,114 +1,100 @@
-/*package whatever //do not write package name here */
-
 import java.io.*;
 import java.util.*;
 
-
 // abstract class
 abstract class Library{
-    abstract int status(String s);
-    abstract void Book_info(int n);
+    abstract int Book_status(String s);
+    abstract int Book_info(int n);
+}
+abstract class Student_Detail{
+       int Student_info(String name, int id){
+          return id;
+      }
 }
 
 
+class Student extends Student_Detail {
+    data_base d = new data_base();
+    int Student_info(String name, int id){
+        return d.student_data(id);
+    }
+}
 
-// libarary info
-class Books extends Library{
+ class data_base{
+    ArrayList <Integer> a = new ArrayList<Integer>();
+    int student_data(int id){
+        a.add(500);
+        a.add(500);
+        a.add(200);
+        a.add(200);
+        library l = new library();
+       int  count =0;
+        for(int i=0;i<a.size();i++) {
+            if (a.get(i) == id)
+                count++;
+        }
+            if(count <2) {
+               a.add(id);
+               count =0;
+                return l.Book_status("1");
+            }
+            else{
+                count =0;
+                return l.Book_status("0");
+            }
 
-    String book_name;
-    String book_author_name;
-    int book_id;
-    int status(String s){
-        if(s=="Student can take book")
-            return 1;
+        }
+     }
+
+class library extends Library {
+
+    ArrayList <String> b = new ArrayList<String>();
+    int  Book_status(String s) {
+        b.add("C");
+        b.add("JAVA");
+        b.add("PYTHON");
+        b.add("C#");
+        if (s == "1") {
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Name the book which you want :");
+            String s1 = sc.nextLine();
+            if (b.contains(s1))
+                return Book_info(1);
+            return Book_info(0);
+        }
         else
+            System.out.println("Not allowed");
             return 0;
     }
-    void Book_info(int n){
-        if(n ==1){
-
-            System.out.println("Book Alloted");
+    int Book_info(int n){
+        //data_base d = new data_base();
+        if(n ==1) {
+            System.out.println("Book  Available");
+            return 1;
         }
-        else{
-            System.out.println("not allowed");
-        }
-    }
-}
-
-
-
-// Student info
-
-class Student{
-    String name;
-    int id;
-    int classs;
-
-    int Student_request(String name,int id, int classs){
-        this.name=name;
-        this.id =id;
-        this.classs=classs;
-        return id;
-    }
-}
-
-
-
-// Librarian info
-class Librarian{
-
-    int max_books;
-    int count =0;
-    int  Student_record(int id){
-        count =0;
-        ArrayList <Integer> a = new ArrayList<Integer>();
-        a.add(500);
-        a.add(508);
-        a.add(550);
-        a.add(500);
-        for(int i=0;i<a.size();i++){
-            if(id ==a.get(i))
-                count ++;
-        }
-        return count;
-
-    }
-
-
-    String max_book(int max_books){
-        this.max_books=max_books;
-
-        if(max_books < 2){
-            return "Student can take book";
-        }
-        else{
-            return "Not allowed,already having two books";
-
-
+        else {
+            System.out.println("Book  Not Available");
+            return 0;
         }
     }
 }
 
 
-//main class
-class java2_1 {
-    public static void main (String[] args) {
-
-
+class java2_1{
+    public static void main(String[] args) {
+        Student s = new Student();
         Student s1 = new Student();
         Student s2 = new Student();
-        int r= s1.Student_request("Pranav",501,12);
+        Student s3 = new Student();
 
-        Librarian l = new Librarian();
-        int lr = l.Student_record(r);
-
-        String ss = l.max_book(lr);
-
-        Books b = new Books();
-        int l1=b.status(ss);
-
-        b.Book_info(l1);
-
+        s.Student_info("A",500);
+        s1.Student_info("B",301);
+        s2.Student_info("C",280);
+        s3.Student_info("D",200);
     }
 }
+
+
+
 
